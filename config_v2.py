3e38c7727efb692faf96b486c042e3903c59bdc4
yinh <115258619+apple-yinhan@users.noreply.github.com>
2025-06-21 17:17:54 +0800

    Add files via upload

---
+++ config_v2.py
@@ -0,0 +1,28 @@
+beats_path = '/home/yinhan/codes/sep4noiseSED/pretrained_models/BEATS_iter3_plus_AS2M.pt'
+w2v2_path = '/home/yinhan/codes/audio_deepfake/networks/wav2vec/xlsr2_300m.pt'
+
+# development
+dev_track1_audio = '/home/yinhan/codes/audio_deepfake/datasets/EnvSDD/released/development'
+dev_track1_meta = '/home/yinhan/codes/envsdd_challenge_2026/metadata/dev_track1.csv'
+
+dev_track2_audio = '/home/yinhan/codes/envsdd_challenge_2026/dataset/dev_track2'
+dev_track2_meta = '/home/yinhan/codes/envsdd_challenge_2026/metadata/dev_track2.csv'
+
+# evaluation
+eval_track1_audio = '/home/yinhan/codes/envsdd_challenge_2026/dataset/eval_track1'
+eval_track1_meta = '/home/yinhan/codes/envsdd_challenge_2026/metadata/eval_track1.csv'
+
+eval_track2_audio = '/home/yinhan/codes/envsdd_challenge_2026/dataset/eval_track2'
+eval_track2_meta = '/home/yinhan/codes/envsdd_challenge_2026/metadata/eval_track2.csv'
+
+
+# test (final ranking)
+test_track1_audio = '/home/yinhan/codes/envsdd_challenge_2026/dataset/test_track1'
+test_track1_meta = '/home/yinhan/codes/envsdd_challenge_2026/metadata/test_track1.csv'
+
+test_track2_audio = '/home/yinhan/codes/envsdd_challenge_2026/dataset/test_track2'
+test_track2_meta = '/home/yinhan/codes/envsdd_challenge_2026/metadata/test_track2.csv'
+
+metadata_json_file = '/home/yinhan/codes/envsdd_challenge_2026/jsons'
+
+
